82b742f6c618cde11ebccb86115a0dcca02075e5
riku <riku2004@gmail.com>
2024-02-03 20:05:15 +0900

    テストコードの見直し・修正

--- japan-population-graph/src/App.test.tsx
+++ japan-population-graph/src/App.test.tsx
@@ -1,45 +1,75 @@
 import { render, screen } from '@testing-library/react';
-import { fetchPrefectures } from './api/fetchPrefectures';
+import { fetchPrefectures, getPrefectureName } from './api/fetchPrefectures';
 import { fetchPrefecturePopulation, type PrefecturePopulation } from './api/fetchPrefecturesPopulation';
 import { fetchPrefecturePopulations } from './api/fetchPrefecturesPopulation';
+import PrefectureCheckBoxList from './parts/prefectureCheckBoxList';
 import PopulationGraph from './parts/populationGraph';
 import App from './App';
-test('fetchPrefectures', async () => {
-  await expect(fetchPrefectures()).resolves.toBeDefined();
+
+// 都道府県を取得する関数
+describe('fetchPrefectures', () => {
+  test('success fetchPrefectures', async () => {
+    const result = await fetchPrefectures();
+    expect(result).toBeDefined();
+    expect(result.result.length).toBe(47);
+    expect(result.result[0]).toHaveProperty('prefCode');
+    expect(result.result[0]).toHaveProperty('prefName');
+  });
+  test('error fetchPrefectures', async () => {
+    // モックを使ってfetchPrefecturesのエラーをテストしたい（mockの実装につまずいているので後回し）
+  });
 });
 
-test('fetchPrefecturePopulation', async () => {
-  const result = await fetchPrefecturePopulation(1);
-  // resultがundefinedでないことを確認
-  expect(result).toBeDefined();
-  expect(result.prefCode).toBe(1);
-  expect(result.populationResponse).toBeDefined();
-  expect(result.populationResponse.result).toBeDefined();
-  expect(result.populationResponse.result.boundaryYear).toBeDefined();
-  expect(result.populationResponse.result.data).toBeDefined();
-  expect(result.populationResponse.result.data[0].label).toBeDefined();
-  expect(result.populationResponse.result.data[0].data).toBeDefined();
-  expect(result.populationResponse.result.data[0].data[0].year).toBeDefined();
-  expect(result.populationResponse.result.data[0].data[0].value).toBeDefined();
+// 都道府県コードから名前を取得する関数
+describe('getPrefectureName', () => {
+  const prefectures = [
+    { prefCode: 1, prefName: '北海道' },
+    { prefCode: 2, prefName: '青森県' },
+  ];
+
+  test('success getPrefectureName', () => {
+    const result = getPrefectureName(1, prefectures);
+    expect(result).toBe('北海道');
+  });
+
+  test('error getPrefectureName', () => {
+    expect(() => getPrefectureName(99, prefectures)).toThrow('都道府県が見つかりませんでした。');
+  });
 });
 
-test('fetchPrefecturePopulations', async () => {
-  const result = await fetchPrefecturePopulations([1, 2]);
-  expect(result).toBeDefined();
-  expect(result.length).toBe(2);
-  expect(result[0].prefCode).toBe(1);
-  expect(result[1].prefCode).toBe(2);
-  expect(result[0].populationResponse).toBeDefined();
-  expect(result[1].populationResponse).toBeDefined();
+// 都道府県の人口を取得する関数
+describe('fetchPrefecturePopulation', () => {
+  test('success fetchPrefecturePopulation', async () => {
+    const result = await fetchPrefecturePopulation(1);
+    expect(result).toBeDefined();
+    expect(result.prefCode).toBe(1);
+    expect(result.populationResponse).toBeDefined();
+    expect(result.populationResponse.result).toHaveProperty('boundaryYear');
+    expect(result.populationResponse.result).toHaveProperty('data');
+  });
+  test('error fetchPrefecturePopulation', async () => {});
 });
 
-test('Appが表示されるか', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/React App/i);
-  expect(linkElement).toBeInTheDocument();
+// 複数の都道府県の人口を取得する関数
+describe('fetchPrefecturePopulations', () => {
+  test('success fetchPrefecturePopulations', async () => {
+    const result = await fetchPrefecturePopulations([1, 2]);
+    expect(result).toBeDefined();
+    expect(result.length).toBe(2);
+    expect(result[0].prefCode).toBe(1);
+    expect(result[1].prefCode).toBe(2);
+    expect(result[0].populationResponse).toBeDefined();
+    expect(result[1].populationResponse).toBeDefined();
+  });
+  test('error fetchPrefecturePopulations', async () => {});
 });
 
-test('PopulationGraphが表示されるか', () => {
+test('render App', () => {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+describe('PopulationGraph', () => {
   const mockPrefectures = [
     { prefCode: 1, prefName: '北海道' },
     { prefCode: 2, prefName: '青森県' },
@@ -87,9 +117,51 @@ test('PopulationGraphが表示されるか', () => {
     },
   ];
 
-  render(<PopulationGraph prefecturePopulations={mockPopulations} prefectures={mockPrefectures} />);
+  test('snapshot PopulationGraph', () => {
+    const { asFragment } = render(
+      <PopulationGraph prefecturePopulations={mockPopulations} prefectures={mockPrefectures} />,
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+  test('render PopulationGraph', () => {
+    render(<PopulationGraph prefecturePopulations={mockPopulations} prefectures={mockPrefectures} />);
+    // HighchartsReactが表示されているか
+    const highchartsElement = screen.getByTestId('highcharts-container');
+    expect(highchartsElement).toBeInTheDocument();
+    expect(highchartsElement).toHaveTextContent('総人口');
+    expect(highchartsElement).toHaveTextContent('北海道');
+    expect(highchartsElement).toHaveTextContent('1980');
+  });
+});
 
-  // HighchartsReactが表示されているか
-  const highchartsElement = screen.getByTestId('highcharts-container');
-  expect(highchartsElement).toBeInTheDocument();
+describe('PrefectureCheckBoxList', () => {
+  const mockPrefectures = [
+    { prefCode: 1, prefName: '北海道' },
+    { prefCode: 2, prefName: '青森県' },
+  ];
+  const mockSelectedPrefectures = [{ prefCode: 1, prefName: '北海道' }];
+  const mockSetSelectedPrefectures = jest.fn();
+  test('snapshot prefectureCheckBoxList', () => {
+    const { asFragment } = render(
+      <PrefectureCheckBoxList
+        prefectures={mockPrefectures}
+        selectedPrefectures={mockSelectedPrefectures}
+        setSelectedPrefectures={mockSetSelectedPrefectures}
+      />,
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+  test('render prefectureCheckBoxList', () => {
+    render(
+      <PrefectureCheckBoxList
+        prefectures={mockPrefectures}
+        selectedPrefectures={mockSelectedPrefectures}
+        setSelectedPrefectures={mockSetSelectedPrefectures}
+      />,
+    );
+
+    // チェックボックスが表示されているか
+    const checkboxElement = screen.getByRole('checkbox', { name: '北海道' });
+    expect(checkboxElement).toBeInTheDocument();
+  });
 });
